237dfa5b7ce6677387bc0cfb63ccb607ceaa7b33
Rithika2284 <vemularithika2284@gmail.com>
2025-08-31 22:47:40 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -15,5 +15,5 @@ public class Factorial {
     public static void main(String[] args) {
         int num = 5; // number to find factorial of
         System.out.println("Factorial of " + num + " is: " + compute(num));
-    }
+    }
 }
